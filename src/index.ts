90575633a58c00533e6becb44a71aa87fd0fe192
= <=>
2025-07-03 19:59:00 +0100

    fixed path config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import path, { dirname } from 'path';
+import 'dotenv/config';
+import path from 'path';
 import express, { Express, Application, Request, Response } from 'express';
 
+const CWD = process.cwd();
+const { PORT, HOST } = process.env;
+
 const app : Application = express();
-const port = 3050;
-const pj = path.join;
 
-app.use('/', express.static(pj(dirname(__dirname), 'public')));
+app.use('/', express.static(path.join(CWD, 'public')));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://${HOST}:${PORT}`);
 });
